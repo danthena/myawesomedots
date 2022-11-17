64b34bf0e1c26d19d9e6154622cec8d8d9deae14
Dan <dan@debian11virt.local.lan>
2022-11-16 19:39:06 -0500

    initial directory sync

---
+++ PostInstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# chmod +x ~/myawesomedots/PostInstall.sh
+
+## Getting dracula gtk theme
+cd ~
+wget https://github.com/dracula/gtk/archive/master.zip
+mkdir ~/.themes
+file-roller gtk-master.zip --extract-to=~/.themes
+cp ~/.themes/gtk-master -r ~/.themes/dracula
+rm -rf ~/.themes/gtk-master 
+
+## Getting Dracula Icon theme
+cd ~
+mkdir /.icons
+cd .icons
+wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+file-roller Dracula.zip --extract-here
+rm Dracula.zip
+
+# Download dots
+echo "Downloading dotfiles"
+cd ~
+git clone https://github.com/CUFTA22/dotfiles
+
+# Symlink dots
+awesomedots=$HOME/myawesomedots
+
+ln -s $awesomedots/xfce4   ~/.config
+ln -s $awesomedots/autostart   ~/.config
+ln -s $awesomedots/awesome    ~/.config
+ln -s $awesomedots/picom      ~/.config
+ln -s $awesomedots/conky       ~/.config
+ln -s $awesomedots/Wallpaper       ~/Wallpaper
+ln -s $awesomedots/Xresources       ~/.Xresources
---
+++ PreInstall.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# chmod +x ~/myawesomedots/PreInstall.sh
+
+echo "Initial apt update"
+LANG=C apt update
+
+echo "Apt Dist Upgrade"
+LANG=C apt dist-upgrade -y
+echo "Dist Upgrade complete. Autoremoving old stuff"
+LANG=C apt autoremove -y
+
+PKGS=(
+
+## Base Install plus
+'xserver-xorg'
+##'lightdm'
+##'lightdm-gtk-greeter'
+'desktop-base'
+'lxdm'
+'git'
+'wget'
+'curl'
+'htop'
+'apt-transport-https'
+'at-spi2-core'
+'psmisc'
+'lxpolkit'
+
+## Desktop Envs
+'awesome'
+'awesome-extra'
+'suckless-tools'
+
+## Networking
+'network-manager-gnome'
+
+## Sound
+'pipewire'
+'pipewire-alsa'
+'pipewire-pulse'
+'alsa-utils'
+'pasystray'
+'playerctl'
+'volumeicon-alsa'
+
+## Rice
+'xfce4-terminal'
+'picom'
+'rofi'
+##'papirus-icon-theme'
+'fonts-font-awesome'
+
+## Goodies
+'firefox-esr'
+'lxappearance'
+'lxrandr'
+'xfce4-panel'
+'xfce4-power-manager'
+'libnotify-bin'
+'xfce4-notifyd'
+'xfce4-goodies'
+'xfce4-power-manager-plugins'
+'xfce4-screenshooter'
+'xfce4-statusnotifier-plugin'
+'xfce4-taskmanager'
+'xfce4-whiskermenu-plugin'
+'xfce4-indicator-plugin'
+'xfce4-session'
+'mousepad'
+'ristretto'
+'synaptic'
+'brightnessctl'
+'thunar'
+'thunar-volman'
+'thunar-archive-plugin'
+'thunar-media-tags-plugin'
+'file-roller'
+'vlc'
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "Installing: ${PKG}"
+    LANG=C apt install "$PKG" -y --no-install-recommends
+done
+echo "All Normal Packages Installed"
+
+# Enable LightDM on boot
+#LANG=C systemctl enable lightdm
+
+echo "Installing Brave Keyring"
+## Install brave browser
+LANG=C curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+LANG=C apt update
+
+echo "Installing Brave-Browser"
+LANG=C apt install brave-browser -y
